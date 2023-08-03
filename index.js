e9d7fe1e54c1f74e86c113e159170037415cd95f
Pete Sharma <protogdyt@gmail.com>
2023-08-02 21:43:42 -0700

    cleanup and comments in index.js

--- index.js
+++ index.js
@@ -7,17 +7,20 @@ const port = 3000
 const connectonService = require('./backend/connectionService')
 const fiboCalc = require('./backend/fibonacciCalculator')
 
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 const conn = connectonService.connectToDB()
 
+// display index page
 app.get('/', (req, res) => {
     console.log('inside display index')
     res.render('index')
 })
 
+// handelling form submission and redirection
 app.post('/fibonacci', (req, res)=>{
     try{
 
@@ -28,18 +31,19 @@ app.post('/fibonacci', (req, res)=>{
             return res.status(400).json({ error: 'Input numberhas to be positive' });
         }
         
+        // DB will always have one row that holds the largest run
         const sqlQuery = "SELECT * FROM sequences"
         conn.query(sqlQuery, (error, existingRow)=>{
             
             if (error) throw error
 
             if(existingRow.length > 0){
-                console.log('we have an existing row')
+                // checking if we have a row in the DB
                 let existingNum = existingRow[0].num
                 let existingSequence = existingRow[0].sequence
-    
+                
                 if(n>existingNum){
-                    console.log('input is greater than calculations before')
+                    // if input is greater than previous calculations perform calculations and update the DB with new calculations
                     const newFiboSeq = fiboCalc.computePartialFiboSequence(existingSequence.split(','), n)
                     const comaSeperatedNewFiboSeq = newFiboSeq.join(', ')
                     const deleteQuery = 'DELETE FROM sequences WHERE num = ?'
@@ -52,25 +56,17 @@ app.post('/fibonacci', (req, res)=>{
                     })
                 }
                 else if (n < existingNum) {
+                    // if input is lesser than previous calculations use previous calculations
                     console.log('input is smaller than calculations before')
-                    // const prevSeq = fiboCalc.fetchComputedFiboSequence(existingSequence.split(','), n)
-                    // const comaSeperatedPrevSeq = prevSeq.join(', ')
-                    // const deleteQuery = 'DELETE FROM sequences WHERE num = ?'
-                    
-                    // conn.query(deleteQuery,[existingNum])
-                    
-                    // const insertNewQuery = 'INSERT INTO sequences (num, sequence) VALUES (?, ?)'
-                    // conn.query(insertNewQuery, [n, comaSeperatedPrevSeq], (err, results) => {
-                    //     if (err) throw err
-                    //     res.redirect(`/fibonacci/${n}`)
-                    // })
                     res.redirect(`/fibonacci/lesser/${n}`)
                 }
-                else if ( n == existingNum ) {
+                else if (n == existingNum) {
+                    // if input matches the stored calculation in the DB use the same
                     console.log('input matched a previously calculated sequence')
                     res.redirect(`/fibonacci/${existingNum}`)
                 }
             } else {
+                // if DB is empty perform full sequence calculation
                 console.log('no previous row found, calculationg fresh')
                 const fullFiboSeq = fiboCalc.computeFullFiboSequence(n)
                 const commaSeperatedfullFiboSeq = fullFiboSeq.join(', ')
@@ -87,6 +83,7 @@ app.post('/fibonacci', (req, res)=>{
     }
 })
 
+// displaying fibonacci numbers if input is greater than or equal to the stored number or if DB is empty
 app.get('/fibonacci/:n', (req,res) => {
     console.log('inside display fibo')
     const n = parseInt(req.params.n, 10)
@@ -100,6 +97,7 @@ app.get('/fibonacci/:n', (req,res) => {
     })
 })
 
+// displaying fibonacci numbers if the input is lesser than the stored number in the DB
 app.get('/fibonacci/lesser/:n', (req,res) => {
     console.log("inside display lesser fibo")
     const n = parseInt(req.params.n, 10)
